a86d456025f8f40f32cf9946c92d7708e520223a
shermansplanet <lsherman@mit.edu>
2021-10-27 23:24:01 -0700

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+const firebaseApp = initializeApp({
+  apiKey: 'AIzaSyBtJKxBqyLefYsVKIqOb-ln96cNQXW00vA',
+  authDomain: 'star-yonder.firebaseapp.com',
+  projectId: 'star-yonder',
+});
+
+const db = getFirestore();
 
 export default function App() {
   return (
