091121a0f3fee2ea9548de3d86b697c8fd55c443
Loren Sherman <loren@nianticspatial.com>
2025-07-20 23:03:20 -0700

    Revert "Revert "3 files modified""
    This reverts commit c13fcdca003a4c9a538ddf110c74a741e1c4b8ec.

--- src/BaseSite.js
+++ src/BaseSite.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import FirebaseImage from './FirebaseImage';
 import Login from './account/Login';
 import SpaceLayout from './space/SpaceLayout';
 import OrbitLayout from './orbit/OrbitLayout';
@@ -90,7 +91,12 @@ export default class BaseSite extends React.Component {
     if (this.state.showLogin) {
       content = <Login />;
     } else if (tab == 'ARCHIVE') {
-      content = <Archive currentData={this.state.currentData} />;
+      content = (
+        <Archive
+          currentData={this.state.currentData}
+          userId={this.state.user == null ? '' : this.state.user.uid}
+        />
+      );
     } else if (tab == 'ABOUT') {
       content = (
         <div className="centered" style={{ top: '80px' }}>
@@ -110,14 +116,22 @@ export default class BaseSite extends React.Component {
             >
               Blender 3D
             </a>{' '}
-            . The current cycle is three hours of voting, then one hour of
-            "travel time" while the result is being generated (a cautious
-            over-estimate; generation rarely takes more than 15 minutes).
+            . Users can vote to decide what area gets explored next. The current
+            cycle is three hours of voting, then one hour of "travel time" while
+            the result is being generated and rendered (which actually only
+            takes a few minutes).
             <br />
             <br />
             The planet map holds data such as humidity, temperature, altitude,
             and density of various plant types, which are then taken into
             account while rendering scenes in a specific location.
+            <div style={{ margin: '10px -70px' }}>
+              <FirebaseImage
+                src={'media/composite.jpg'}
+                height={510}
+                width={570}
+              />
+            </div>
           </div>
         </div>
       );
@@ -167,31 +181,6 @@ export default class BaseSite extends React.Component {
             onClick={() => this.setState({ tab: 'ABOUT', showLogin: false })}
           >
             About
-          </button>
-          <button
-            className="tabButton"
-            style={{
-              paddingLeft: '8px',
-              paddingRight: '5px',
-              paddingBottom: '5px',
-              paddingTop: '33px',
-            }}
-            onClick={() => window.open('https://twitter.com/FarFloraBot')}
-          >
-            <svg
-              version="1.1"
-              baseProfile="full"
-              width="39"
-              height="36"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill="white"
-                transform="translate(-35,-52), scale(0.15)"
-                id="path5"
-                d="m 453.82593,412.80619 c -6.3097,2.79897 -13.09189,4.68982 -20.20852,5.54049 7.26413,-4.35454 12.84406,-11.24992 15.47067,-19.46675 -6.79934,4.03295 -14.3293,6.96055 -22.34461,8.53841 -6.41775,-6.83879 -15.56243,-11.111 -25.68298,-11.111 -19.43159,0 -35.18696,15.75365 -35.18696,35.18525 0,2.75781 0.31128,5.44359 0.91155,8.01875 -29.24344,-1.46723 -55.16995,-15.47582 -72.52461,-36.76396 -3.02879,5.19662 -4.76443,11.24048 -4.76443,17.6891 0,12.20777 6.21194,22.97747 15.65332,29.28716 -5.76773,-0.18265 -11.19331,-1.76565 -15.93716,-4.40083 -0.004,0.14663 -0.004,0.29412 -0.004,0.44248 0,17.04767 12.12889,31.26806 28.22555,34.50266 -2.95247,0.80436 -6.06101,1.23398 -9.26989,1.23398 -2.2673,0 -4.47114,-0.22124 -6.62011,-0.63114 4.47801,13.97857 17.47214,24.15143 32.86992,24.43441 -12.04227,9.43796 -27.21366,15.06335 -43.69965,15.06335 -2.84014,0 -5.64082,-0.16722 -8.39349,-0.49223 15.57186,9.98421 34.06703,15.8094 53.93768,15.8094 64.72024,0 100.11301,-53.61524 100.11301,-100.11387 0,-1.52554 -0.0343,-3.04251 -0.10204,-4.55261 6.87394,-4.95995 12.83891,-11.15646 17.55618,-18.21305 z"
-              />
-            </svg>
           </button>
         </div>
         {voteUpdated ? (
